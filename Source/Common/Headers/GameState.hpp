5bb0999bd8f3bf62860009142afd2c26ca471eb7
Rico <teh_programerer@interactivelaboratories.com>
2013-12-18 16:42:00 +0000

    |SOURCE| [Common::GameState] * GameState is a class with no funcitonality (not an abstract interface, as   there will be attributes) which is intended to be overridden in order to be   of any use * Game states can be entered, executed, and, ultimately, exited, the derived   classes define this behaviour * The GameStateManager will use the return values from the Enter, Execute, and   Exit member funcitons to determine if an error should be reported back to the   user

---
+++ Source/Common/Headers/GameState.hpp
@@ -0,0 +1,24 @@
+#ifndef __PAPANAK_GAMESTATE_HPP__
+#define __PAPANAK_GAMESTATE_HPP__
+
+#include <System/DataTypes.hpp>
+
+namespace Papanak
+{
+	class GameStateManager;
+
+	class GameState
+	{
+	public:
+		virtual ~GameState( );
+
+		virtual ZED_UINT32 Enter( GameStateManager * const &p_pManager ) = 0;
+		virtual ZED_UINT32 Execute( GameStateManager * const &p_pManager ) = 0;
+		virtual ZED_UINT32 Exit( GameStateManager * const &p_pManager ) = 0;
+
+	protected:
+	};
+}
+
+#endif
+
---
+++ Source/Common/Source/GameState.cpp
@@ -0,0 +1,9 @@
+#include <GameState.hpp>
+
+namespace Papanak
+{
+	GameState::~GameState( )
+	{
+	}
+}
+
